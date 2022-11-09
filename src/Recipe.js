9078957dc314dbebc2e9faa1b2bb045643460e3d
mythily-muthu <mythilimuthu18@gmail.com>
2022-11-09 18:07:57 +0530

    Recipe app with react

---
+++ src/Recipe.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import style from './recipe.module.css';
+
+const Recipe = ({ title, cuisineType, calories, image, ingredients }) => {
+
+    return (
+
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <h2>{cuisineType} cuisine</h2>
+            <h3>{calories} Calories</h3>
+            <ul>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
+            <img className={style.image} src={image} alt="chicken_recipe_image" />
+
+        </div>
+    )
+}
+
+export default Recipe
